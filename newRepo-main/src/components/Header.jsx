2a14878e1f48bc77c2b83225a383488badce9677
BurnedIsaac <coolbelkin@gmail.com>
2022-06-18 16:56:05 +0300

    added new file newRepo-main

---
+++ newRepo-main/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+      return <header className="Header">
+      <img
+        width={82}
+        src="https://st.depositphotos.com/1924257/4367/i/600/depositphotos_43672555-stock-photo-abstract-colorful-leaves-tree-happy.jpg"
+      />
+    </header>
+
+}
+
+export default Header; 
---
+++ newRepo-main/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+     return <div className="content">
+     <img
+       width={1000}
+       height={300}
+       src="https://assets.gq.ru/photos/5d9f65b2f09bda0008040db8/16:9/w_2560%2Cc_limit/b54f734c97173d3ca9b891267f4bd8c2c0ea3430.jpeg"
+     />
+     <div>ava</div>
+     <div>my post</div>
+     <div>new post</div>
+     <div>post 1</div>
+     <div>post 2</div>
+   </div>
+
+}
+
+export default Profile;
